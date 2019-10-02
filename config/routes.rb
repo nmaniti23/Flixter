8924e70471de3502e71809d3202b0df8b0d6e74f
Nathaniel <n.maniti@gmail.com>
2019-10-02 17:13:58 +0000

    add courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
+  resources :courese, only: [:index, :show]
+  namespace :instructor do
+  	resources :courses, only: [:new, :create, :show]
+  end
 end
