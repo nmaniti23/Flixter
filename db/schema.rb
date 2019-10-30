f7b3fd7f4ae72e1eeed092a474f36b8ec3c4efb8
Nathaniel <n.maniti@gmail.com>
2019-10-30 20:44:44 +0000

    add corrections

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_015334) do
+ActiveRecord::Schema.define(version: 2019_10_16_034229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "courses", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "cost"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
   create_table "lessons", force: :cascade do |t|
@@ -45,9 +50,6 @@ ActiveRecord::Schema.define(version: 2019_10_19_015334) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.json "avatars"
-    t.string "avatar"
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
